3889d1e7668827c2de7593c9bc159517e4e98b99
Kristen Khong <31214581+kristenkhong@users.noreply.github.com>
2021-04-13 18:35:34 +0800

    after - new

--- work.dae09d53.js
+++ work.dae09d53.js
@@ -326,7 +326,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53793" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57967" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
